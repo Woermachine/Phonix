6f73e9478dc404b14edecf3025fdd5d31014430b
Woermachine <stephenwoerner@gmail.com>
2017-10-05 19:31:26 -0400

    CodySucks.py

--- Properties.py
+++ Properties.py
@@ -1,16 +1,14 @@
-#default property values
+# Default property values
 DEFAULT_SCROLL_SPEED = 1.0
-DEFAULT_VOLUME = 1
 DEFAULT_THRESHOLD = 1.0
 DEFAULT_HEADPHONES_ON = False
 DEFAULT_EA_ON = True
 DEFAULT_SHOTGUN_ON = True
 
 scrollSpeed = DEFAULT_SCROLL_SPEED
-volume = DEFAULT_VOLUME
 threshold = DEFAULT_THRESHOLD
 headphonesOn = DEFAULT_HEADPHONES_ON
-EAOn = DEFAULT_EA_ON
-shotgunOn = DEFAULT_SHOTGUN_ON
+EAOn = DEFAULT_EA_ON # Variable may not be necessary - Woermachine
+shotgunOn = DEFAULT_SHOTGUN_ON  # Variable may not be necessary - Woermachine
 
-#getters and setters for each property
+# TODO: getters and setters for each property
--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # Phonix
 
-This repository contains the software which will run on the Raspberry Pi and Arduino. It will compose the Phonic smartglasses software.
+This repository contains the software which will run on the Raspberry Pi and Arduino. It will compose the Phonix smartglasses software.
 
+# Setup
 
 The following are the pin numbers counting from the sd card side used to wire the display: these are the actual numbers starting at 1 not what diagrams on wiring will say. 
 
