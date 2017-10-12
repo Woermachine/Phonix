0bee187bd94bd6e51cef0538391391e5fffb8de7
Jason Schafer <jason59549@gmail.com>
2017-10-12 06:31:33 -0400

    First Iteration of Bluetooth Module Finished
    Not pretty, but hey it works.

--- Bluetooth.py
+++ Bluetooth.py
@@ -3,7 +3,7 @@ from bluetooth import *
 
 bufferIn = []
 bufferOut = []
-
+running = True
 def onReceiveAudio(audio):
     #puts audio in the bufferOut
     return
@@ -12,8 +12,10 @@ def sendAudio():
     #sends audio from buffer to phone
     return
 
-def onReceiveText( text):
+def onReceiveText(text):
     #receive text from phone and puts it in bufferIn
+    print("received [%s]" % text)
+    bufferIn.append(text)
     return
 
 def sendText():
@@ -22,12 +24,12 @@ def sendText():
 
 
 server_sock = BluetoothSocket(RFCOMM)
-server_sock.bind(("", PORT_ANY))
-server_sock.listen(1)
+server_sock.bind(("", PORT_ANY)) #bind to any available port
+server_sock.listen(1) #Listen for connections
 
-port = server_sock.getsockname()[1]
+port = server_sock.getsockname()[1] #Get Port
 
-uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
+uuid = "b8162268-3e67-4749-9508-cb30b86703c7"
 
 advertise_service(server_sock, "PhonixServer",
                   service_id=uuid,
@@ -36,18 +38,22 @@ advertise_service(server_sock, "PhonixServer",
                   #                   protocols = [ OBEX_UUID ]
                   )
 
-print("Waiting for connection on RFCOMM channel %d" % port)
-
-client_sock, client_info = server_sock.accept()
-print("Accepted connection from ", client_info)
-
-try:
-    while True:
-        data = client_sock.recv(1024)
-        if len(data) == 0: break
-        print("received [%s]" % data)
-except IOError:
-    pass
+connected = True
+while running:
+    while ~connected:
+        print("Waiting for connection on RFCOMM channel %d" % port)
+        client_sock, client_info = server_sock.accept()
+        print("Accepted connection from ", client_info)
+        connected = True
+        try:
+            while connected:
+                data = client_sock.recv(1024)
+                if len(data) != 0:
+                    onReceiveText(data)
+                else:
+                    connected = False
+        except IOError:
+            connected = False
 
 print("disconnected")
 
