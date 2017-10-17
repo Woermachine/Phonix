f42c4e26e10c3e283ffeb5f42d7d4ed3cec69f44
Jacob Jesionowski <jacob.a.jesionowski@leidos.com>
2017-10-17 15:28:08 -0400

    Updated properties class with reset to default and a comment explaining how properties can be accessed

--- Properties.py
+++ Properties.py
@@ -5,19 +5,16 @@ DEFAULT_HEADPHONES_ON = False
 DEFAULT_EA_ON = True
 DEFAULT_SHOTGUN_ON = True
 
+#These can all be accessed directly after importing Properties
 scrollSpeed = DEFAULT_SCROLL_SPEED
 threshold = DEFAULT_THRESHOLD
 headphonesOn = DEFAULT_HEADPHONES_ON
 EAOn = DEFAULT_EA_ON # Variable may not be necessary - Woermachine
 shotgunOn = DEFAULT_SHOTGUN_ON  # Variable may not be necessary - Woermachine
 
-class Properties:
-    def __init__(self, scrollspeed, volume, threshold, headphones, ea):
-        self.s = scrollspeed
-        self.v = volume
-        self.t = threshold
-        self.h = headphones
-        self.e = ea
-
-
-# TODO: getters and setters for each property
+def resetToDefault():
+    scrollSpeed = DEFAULT_SCROLL_SPEED
+    threshold = DEFAULT_THRESHOLD
+    headphonesOn = DEFAULT_HEADPHONES_ON
+    EAOn = DEFAULT_EA_ON  # Variable may not be necessary - Woermachine
+    shotgunOn = DEFAULT_SHOTGUN_ON  # Variable may not be necessary - Woermachine
